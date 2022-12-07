4e3e84fe0e5da78ed8987ead3f4c2d76419159d8
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-06 14:01:20 -1000

    basic full-stack app set up

---
+++ server/fetch_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse 
+from rest_framework.decorators import api_view 
+
+def index(request):
+    print('index')
+    homepage = open('static/index.html').read()
+    return HttpResponse(homepage)
