323d4aed9d0fc960438def064c3b6d3b1a295da4
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-07 17:00:04 -1000

    added test to validate min age for app user

--- server/fetch_app/tests.py
+++ server/fetch_app/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+from .models import AppUser
 
-# Create your tests here.
+class AppUserTest(TestCase):
+    def test_01_create_user(self):
+        new_user = AppUser(username='steve@apple.com', password='1234', first_name='Steve', last_name='Jobs', email='steve@apple.com', birthdate='2010-12-08')
+        try:
+            new_user.full_clean()
+            self.fail()
+        except ValidationError as e:
+            self.assertTrue("You must be at least 18 years old to use Fetch." in e.message_dict['birthdate'])
+
