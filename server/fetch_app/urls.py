5cb3b48a9c1e4274a65587e849a2320ba2b082c4
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-08 17:35:52 -1000

    added signup and login urls

--- server/fetch_app/urls.py
+++ server/fetch_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('signup', views.signup),
+    path('login', views.login),
 ]
