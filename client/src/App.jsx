ca8129b50b83aaf8c2fb0ff06c10a0320d88646c
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-08 17:32:14 -1000

    added csrf and IndexPage element

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,29 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import IndexPage from './pages/IndexPage'
+import axios from 'axios';
 
 function App() {
+
+  function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+  const csrftoken = getCookie('csrftoken');
+  axios.defaults.headers.common['X-CSRFToken'] = csrftoken
+
   const [count, setCount] = useState(0)
 
   return (
@@ -12,18 +33,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <IndexPage/>
     </div>
   )
 }
