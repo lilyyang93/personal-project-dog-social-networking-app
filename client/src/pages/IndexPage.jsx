ab9e099f3459bf3906652a67ce0d80f4c067e092
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-09 16:13:20 -1000

    added path to homepage

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,13 +1,14 @@
 import {HashRouter as Router, Routes, Route} from 'react-router-dom'
 import SignUpPage from './SignUpPage'
 import LogInPage from './LogInPage'
+import HomePage from './HomePage'
 //import axios from 'axios'
 
 export default function IndexPage() {
 
     return (
     <div className="IndexPage">
-        <h1>Welcome to Fetch</h1><hr/>
+        <h1>Welcome!</h1><hr/>
         <a href="/#/login">Log In</a>
         <br/><br/>
         <a href="/#/signup">Sign Up</a>
@@ -15,6 +16,7 @@ export default function IndexPage() {
             <Routes>
                 <Route path="signup" element={<SignUpPage />}></Route>
                 <Route path="login" element={<LogInPage />}></Route>
+                <Route path="homepage" element={<HomePage />}></Route>
             </Routes>
         </Router>
     </div>
