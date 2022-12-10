72d41887203276877b0a2a7783d8b9ae032bca73
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-09 16:11:17 -1000

    added login/logout redirect urls but not working

--- server/server/settings.py
+++ server/server/settings.py
@@ -128,6 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'fetch_app.AppUser'
 
-AUTHENTICATION_BACKENDS = (
-'django.contrib.auth.backends.ModelBackend',
-)
+LOGIN_REDIRECT_URL = ""
+LOGOUT_REDIRECT_URL = "/#/login"
