e92429024b5a3897426a282bab63f7674ce637c5
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-09 16:07:46 -1000

    login view now working but does not redirect after login

--- server/fetch_app/views.py
+++ server/fetch_app/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse 
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from rest_framework.decorators import api_view 
 from .models import AppUser
 
+
 def index(request):
-    print('index')
+    print('at the index')
     homepage = open('static/index.html').read()
     return HttpResponse(homepage)
 
@@ -19,23 +20,29 @@ def signup(request):
         first_name = request.data['first_name']
         last_name = request.data['last_name']
 
-        new_user = AppUser.objects.create(username=username, email=email, password=password, birthdate=birthdate, first_name=first_name, last_name=last_name)
+        new_user = AppUser.objects.create(
+            username=username, 
+            email=email, 
+            password=password, 
+            birthdate=birthdate, 
+            first_name=first_name, 
+            last_name=last_name
+        )
+        new_user.set_password(password)
         new_user.save()
     return JsonResponse({"success":True})
 
 @api_view(["GET", "POST"])
-def login(request):
+def login_user(request):
     if request.method == "POST":
-        email = request.data['username']
+        email = request.data['email']
         password = request.data['password']
-        print(email, password)
-        user = authenticate(request, username=email, password=password)
-        print(user)
+        user = authenticate(username=email, password=password)
         if user is not None:
             if user.is_active:
                 try: 
                     login(request, user)
-                    return JsonResponse({'success': True})
+                    return JsonResponse({'success':True})
                 except Exception as e:
                     return JsonResponse({'success': False, 'reason':'login failed'})
             else:
@@ -43,3 +50,12 @@ def login(request):
         else:
             return JsonResponse({'success': False, 'reason': 'account does not exist'})
     return JsonResponse({'success':True})
+
+@api_view
+def logout_user(request):
+    pass
+
+@api_view 
+@login_required
+def homepage(request):
+    print(request.user.email)
