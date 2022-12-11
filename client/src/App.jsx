7dde8ba456a7ce85113c70a5b398e0fcf62ec1fe
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-10 15:07:04 -1000

    updated routes and functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-import IndexPage from './pages/IndexPage'
-import axios from 'axios';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import SignUpPage from './pages/SignUpPage'
+import HomePage from './pages/HomePage'
+import LogInPage from './pages/LogInPage';
 
 function App() {
-
-  function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-  const csrftoken = getCookie('csrftoken');
-  axios.defaults.headers.common['X-CSRFToken'] = csrftoken
-
-  const [count, setCount] = useState(0)
-
   return (
+    <Router>
     <div className="App">
-      <div>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div className="card">
-      </div>
-      <IndexPage/>
+      <Routes>
+        <Route path="" element={<LogInPage />}> </Route>
+        <Route path="signup" element={<SignUpPage />}></Route>
+        <Route path="homepage" element={<HomePage />}></Route>
+      </Routes>
     </div>
+    </Router>
   )
 }
 
