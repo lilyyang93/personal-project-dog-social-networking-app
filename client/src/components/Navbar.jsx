ec96972130e09b05d5d49cd2cb07db82b75b0650
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-11 17:08:08 -1000

    logout function working but not redirecting

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,23 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import axios from 'axios';
-
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
 
 export default function NavBar() {
-    
+
+    const [userLoggedIn, setUserLoggedIn] = useState(true)
+
     async function logoutUser() {
-        let response = await axios.post("logout", {
-            "user" : user
-        })
+        let response = await axios.post("logout")
         if (response.data.success) {
-            console.log('user successfully logged out in')
+            console.log('user successfully logged out')
+            setUserLoggedIn(false)
         }
+    }
+
+    if (!userLoggedIn) {
+        return <Navigate to=""/>
     }
 
     return (
