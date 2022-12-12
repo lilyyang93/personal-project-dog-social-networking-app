b951101f4ceb6f3ab26a1dd4d1958c5d88b3fb4c
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-11 17:09:05 -1000

    changed quotes to double

--- server/fetch_app/urls.py
+++ server/fetch_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.index),
-    path('signup', views.signup),
-    path('homepage', views.homepage),
-    path('logout', views.logout_user),
+    path("", views.index),
+    path("signup", views.signup),
+    path("homepage", views.homepage),
+    path("logout", views.logout_user),
 ]
