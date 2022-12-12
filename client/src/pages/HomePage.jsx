7804b84cf8dc1df69edac1c93b1b21910a56fd37
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-11 17:08:39 -1000

    no major changes

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,20 @@
+import axios from "axios"
 import NavBar from "../components/Navbar"
+import { useEffect } from "react"
 
 export default function HomePage() {
+
+    useEffect(()=> {
+        getUserPage
+    },[])
+
+
+    async function getUserPage() {
+        let response = await axios.get("homepage")
+    }    
+
     return (
-        <div className="NavBarComponent">
+        <div className="homepage">
             <NavBar />
             <h1> welcome to your homepage </h1>
         </div>
