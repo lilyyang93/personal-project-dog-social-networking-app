3bdae1b9fcd541a48a286fd03cc091322748af45
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-17 10:07:17 -1000

    installed oauthlib, dotenv, json, get request to noun project API for image url

--- server/fetch_app/views.py
+++ server/fetch_app/views.py
@@ -2,7 +2,14 @@ from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from rest_framework.decorators import api_view 
-from .models import AppUser
+from .models import *
+import requests as client_request
+from requests_oauthlib import OAuth1
+from dotenv import load_dotenv
+import os 
+import json
+
+load_dotenv()
 
 @api_view(["GET", "POST"])
 def index(request):
@@ -35,6 +42,7 @@ def signup(request):
         birthdate = request.data["birthdate"]
         first_name = request.data["first_name"] 
         last_name = request.data["last_name"]
+        city = request.data["city"]
 
         new_user = AppUser.objects.create(
             username=username, 
@@ -42,7 +50,9 @@ def signup(request):
             password=password, 
             birthdate=birthdate, 
             first_name=first_name, 
-            last_name=last_name
+            last_name=last_name,
+            city=city,
+            image="human",
         )
         new_user.set_password(password)
         new_user.save()
@@ -57,9 +67,17 @@ def logout_user(request):
 @login_required
 def homepage(request):
     if request.method == "GET":
+        auth = OAuth1(os.environ['apikey'], os.environ['secretkey'])
+
         my_user = request.user.username
         logged_in_user = AppUser.objects.get(username=my_user)
+        NP_API_response = client_request.get(f"http://api.thenounproject.com/icon/{logged_in_user.image}", auth=auth)
+        responseJSON = NP_API_response.json()
+        image_url = responseJSON['icon']['preview_url']
         return JsonResponse({
             "firstname":logged_in_user.first_name,
             "email": logged_in_user.username,
+            "location": logged_in_user.city,
+            "image": logged_in_user.image,
+            "image_url": image_url
         })
