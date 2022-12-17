8dd966543aded0c15036b0ca73d0d6dc320d3aaf
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-17 10:08:31 -1000

    created component for a profile teaser to load on homepage upon login

---
+++ client/src/components/ProfileTeaser.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import {
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+  MDBCard,
+  MDBCardText,
+  MDBCardBody,
+  MDBCardImage,
+  MDBBtn,
+} from 'mdb-react-ui-kit';
+
+export default function ProfileTeaser(props) {
+    return (
+        <div className="ProfileTeaser">
+            <section style={{ backgroundColor: '#eee' }}>
+      <MDBContainer className="py-5">
+        <MDBRow>
+          <MDBCol lg="4">
+            <MDBCard className="mb-4">
+              <MDBCardBody className="text-center">
+                <MDBCardImage
+                  src={props.image_url}
+                  alt="avatar"
+                  className="rounded-circle"
+                  style={{ width: '150px' }}
+                  fluid />
+                <p className="text-muted mb-1">Name: {props.myUser}</p>
+                <p className="text-muted mb-4">City: {props.myCity}</p>
+                <div className="d-flex justify-content-center mb-2">
+                  <MDBBtn outline className="ms-1">Edit</MDBBtn>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+
+          </MDBCol>
+          <MDBCol lg="8">
+            <MDBCard className="mb-4">
+              <MDBCardBody>
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>View Requests</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">blah</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>View Favorites</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">blah</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>Find Friends</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">blah</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </section>
+        </div>
+    )
+}
