1b35743753dd28ad8de96bb6c8787c00b4423449
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-17 10:03:13 -1000

    axios get request for noun project image url

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
 import axios from "axios"
 import NavBar from "../components/Navbar"
 import { useState, useEffect } from "react"
+import ProfileTeaser from "../components/ProfileTeaser"
 
 export default function HomePage() {
 
     const [myUser, setMyUser] = useState(null)
+    const [myCity, setMyCity] = useState('unknown')
+    const [myImage, setMyImage] = useState(null)
 
     useEffect(()=> {
         getUserPage()
@@ -13,12 +16,17 @@ export default function HomePage() {
     async function getUserPage() {
         let response = await axios.get("homepage")
         setMyUser(response.data.firstname)
+        setMyCity(response.data.location)
+        setMyImage(response.data.image_url)
     }   
 
     return (
         <div className="homepage">
             <NavBar />
+            <br/>
             <h1>Welcome to your homepage, {myUser}</h1>
+            <hr/>
+            <ProfileTeaser myUser = {myUser} myCity = {myCity} image_url = {myImage}/>
         </div>
     )
 }
