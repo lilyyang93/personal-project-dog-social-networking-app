2f72ed890adecd32a35809120483f0bb21337615
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-18 11:15:01 -1000

    imported EditProfilePage and client side route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import SignUpPage from './pages/SignUpPage'
 import HomePage from './pages/HomePage'
 import LogInPage from './pages/LogInPage';
+import EditProfilePage from './pages/EditProfilePage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<LogInPage />}> </Route>
         <Route path="/signup" element={<SignUpPage />}></Route>
         <Route path="/homepage" element={<HomePage />}></Route>
+        <Route path="/editprofile" element={<EditProfilePage />}></Route>
       </Routes>
     </div>
     </Router>
