e03fbd7430295d22f4d79ec095b76238b82dc751
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-21 14:53:05 -1000

    retrieve pet name data and send to ProfileTeaser component

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -8,6 +8,7 @@ export default function HomePage() {
     const [myUser, setMyUser] = useState(null)
     const [myCity, setMyCity] = useState('unknown')
     const [myImage, setMyImage] = useState(null)
+    const [myPets, setMyPets] = useState([])
 
     useEffect(()=> {
         getUserPage()
@@ -18,7 +19,8 @@ export default function HomePage() {
         setMyUser(response.data.firstname)
         setMyCity(response.data.location)
         setMyImage(response.data.image_url)
-    }   
+        setMyPets([response.data.pet_names])
+    }  
 
     return (
         <div className="HomePage">
@@ -26,7 +28,11 @@ export default function HomePage() {
             <br/>
             <h1>Welcome to your homepage, {myUser}</h1>
             <hr/>
-            <ProfileTeaser myUser = {myUser} myCity = {myCity} image_url = {myImage}/>
+            <ProfileTeaser 
+                myUser={myUser} 
+                myCity={myCity} 
+                image_url={myImage}
+                myPets={myPets}/>
         </div>
     )
 }
