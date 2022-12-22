e049fb8bc64d2c02a2a247c4cdad61b68c4125eb
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-22 12:30:50 -1000

    removed break after header line

--- client/src/components/PetProfileTeaser.jsx
+++ client/src/components/PetProfileTeaser.jsx
@@ -1,8 +1,7 @@
 export default function PetProfileTeaser({myPets}) {
     return (
     <div className="PetProfileTeaser">
-        <h2>your pets</h2>
-        <br/><hr/>
+        <h2>your pets</h2><hr/>
         {myPets.map((pet)=>{
             return <h4>{pet}</h4>
         })}
