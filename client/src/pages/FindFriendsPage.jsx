9675522e4b97d31c3b1b3f05773a3e14a2efb63d
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-22 14:45:47 -1000

    successfully display no friends found if no friends in same city

--- client/src/pages/FindFriendsPage.jsx
+++ client/src/pages/FindFriendsPage.jsx
@@ -1,20 +1,29 @@
 import axios from 'axios'
 import NavBar from "../components/Navbar"
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function FindFriendsPage() {
+
+    const [friends, setFriends] = useState([])
+
     useEffect(()=>{
-        hello()
+        getFriends()
     },[])
 
-    async function hello() {
+    async function getFriends() {
         let response = await axios.get("findfriends")
-        console.log(response.data)
+        setFriends(response.data.friends)
+        console.log(friends)
     }  
     return (
         <div className="FindFriendsPage">
             <NavBar /><br/>
-            <h3>fetching friends in your area...</h3>
+            <h3>fetching friends in your area...</h3><br/><br/>
+            {friends.length == 0 ? 
+            <h5>No friends found. Please try again later.</h5> : 
+            friends.map((friend)=> {
+                <h3>{friend}</h3>
+            })}
         </div>
     )
 }
