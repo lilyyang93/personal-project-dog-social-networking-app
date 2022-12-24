26841d97d2c1a7b96374e3aa95001d8d36a1379c
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-24 10:37:11 -1000

    successfuly display card with names of dogs in area

--- client/src/components/FriendTeaser.jsx
+++ client/src/components/FriendTeaser.jsx
@@ -1,7 +1,21 @@
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+
 export default function FriendTeaser(props) {
     return (
-        <div className="FriendTeaser">
-            <h2>{props.friendNames}</h2>
-        </div>
+    <div className="FriendTeaser">
+
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={props.ProfileImages} />
+      <Card.Body>
+        <Card.Title>{props.friendNames}</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+    </div>
     )
 }
