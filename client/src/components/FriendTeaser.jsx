925cb9dc0ba56debe4d3709c3ac90e82c3c5575d
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-24 13:54:47 -1000

    successfully display card with info on each dog in area, image source does not work

--- client/src/components/FriendTeaser.jsx
+++ client/src/components/FriendTeaser.jsx
@@ -1,21 +1,22 @@
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 
-export default function FriendTeaser(props) {
+export default function FriendTeaser({friends}) {
     return (
     <div className="FriendTeaser">
-
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={props.ProfileImages} />
-      <Card.Body>
-        <Card.Title>{props.friendNames}</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    {friends.map((friend)=> (
+     <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={friend.profile_image} />
+        <Card.Body>
+            <Card.Title>{friend.name}</Card.Title>
+            <Card.Text>
+            {friend.personality}
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+        </Card>
+    ))} 
     </div>
     )
 }
+
