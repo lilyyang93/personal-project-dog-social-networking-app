bee727b0f1b1fa735c468f9017e61d7cbc174e57
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-25 13:30:14 -1000

    display owner's pet info on cards, updated to bring in all relevant data from backend

--- client/src/components/PetProfileTeaser.jsx
+++ client/src/components/PetProfileTeaser.jsx
@@ -1,10 +1,35 @@
-export default function PetProfileTeaser({myPets}) {
+import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup';
+
+export default function PetProfileTeaser({pets}) {
+
+    const navigate = useNavigate()
+
     return (
     <div className="PetProfileTeaser">
-        <h2>your pets</h2><hr/>
-        {myPets.map((pet)=>{
-            return <h4>{pet}</h4>
-        })}
+    {pets.map((pet)=> (
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={pet.profile_image} />
+        <Card.Body>
+            <Card.Title>{pet.name}</Card.Title>
+            <Card.Text>
+            DOB: {pet.birthdate}<br/>
+            {pet.gender}<br/>
+            spayed/neutered: {pet.spayed_neutered_status}<br/>
+            breed: {pet.breed}
+            </Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+            <ListGroup.Item>personality: {pet.personality}</ListGroup.Item>
+            <ListGroup.Item>{pet.name} likes {pet.likes}</ListGroup.Item>
+        </ListGroup>
+        <Card.Body>
+            <Card.Link onClick={()=>navigate(`/editpetprofile${pet.id}`)}>edit profile</Card.Link>
+        </Card.Body>
+        </Card>
+    ))} 
     </div>
     )
 } 
