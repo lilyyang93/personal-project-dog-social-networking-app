b02f3f2417f42676f1320ccc4641f71e3b146201
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-26 11:15:45 -1000

    SendMessagePage updated to be able to send a message and save to database, works

--- client/src/pages/SendMessagePage.jsx
+++ client/src/pages/SendMessagePage.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import NavBar from "../components/Navbar"
 import axios from "axios"
+import { MDBTextArea } from 'mdb-react-ui-kit';
+import { MDBInput } from 'mdb-react-ui-kit';
+import Button from 'react-bootstrap/Button'
 
 export default function SendMessagePage() {
     function getCookie(name) {
@@ -23,6 +26,8 @@ export default function SendMessagePage() {
     axios.defaults.headers.common['X-CSRFToken'] = csrftoken
 
     const [myFriend, setMyFriend] = useState([])
+    const [sentMessage, setSentMessage] = useState(false)
+    const [friendID, setFriendID] = useState("")
 
     let { petID } = useParams()
 
@@ -30,19 +35,42 @@ export default function SendMessagePage() {
         getPage()
     },[])
 
+    useEffect(()=>{
+        console.log(friendID)
+    },[friendID])
+
     async function getPage() {
         let response = await axios.get("", {params:{"petID":petID}})
         setMyFriend(response.data)
+        setFriendID(petID)
+        console.log(response.data.name)
     }
 
-    useEffect(()=>{
-        console.log(myFriend)
-    },[myFriend])
+    async function sendMessage() {
+        let title = document.getElementById("title").value
+        let message = document.getElementById("message").value
+
+        let response = await axios.post("", {
+            'petID': friendID,
+            'title': title,
+            'message': message
+        })
+
+        if (response.data.success) {
+            setSentMessage(true)
+        }
+    }
 
     return (
         <div className="SendMessagePage">
             <NavBar /><br/>
             <h3>send a message to {myFriend.name}</h3>
+            <MDBInput label="title" id='title' type='text' /><br/>
+            <MDBTextArea label="send your message!" id='message' rows={4} />
+            <br/> 
+            <Button variant="primary" onClick={()=>sendMessage()}>send</Button>
+            <br/>
+            {sentMessage ? <h4>message sent!</h4> : ""}
         </div>
     )
 }
