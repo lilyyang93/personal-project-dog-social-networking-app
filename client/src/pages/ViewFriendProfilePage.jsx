9926fc280c63358aaf631f37c7b4920df0c4906d
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-26 08:35:54 -1000

    use param of petID to grab data for that pet, and display information. works

--- client/src/pages/ViewFriendProfilePage.jsx
+++ client/src/pages/ViewFriendProfilePage.jsx
@@ -1,7 +1,59 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/Navbar"
+import axios from "axios"
+
 export default function ViewFriendProfilePage(){
+    function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    const csrftoken = getCookie('csrftoken');
+    axios.defaults.headers.common['X-CSRFToken'] = csrftoken
+
+    const [myFriend, setMyFriend] = useState([])
+
+    let { petID } = useParams()
+
+    useEffect(()=>{
+        viewFriend()
+    },[])
+
+    async function viewFriend() {
+        let response = await axios.get("", {params:{"petID":petID}})
+        setMyFriend(response.data)
+    }
+
+    useEffect(()=>{
+        console.log(myFriend)
+    },[myFriend])
+
     return (
         <div className="ViewFriendProfilePage">
-            <h2>friend's page</h2>
+            <NavBar /><br/>
+            <h2>{myFriend.name}</h2>
+            <hr/>
+            <p>DOB: {myFriend.birthdate}</p>
+            <p>{myFriend.gender} {myFriend.breed}</p>
+            <p>spayed/neutered: {myFriend.spayed_neutered}</p>
+            <br/><br/><br/>
+            <h4>{myFriend.name}'s personality:</h4>
+            <p>{myFriend.personality}</p>
+            <br/><br/><br/>
+            <h4>{myFriend.name} likes: </h4>
+            <p>{myFriend.likes}</p>
+            <p>{myFriend.profile_image}</p>
         </div>
     )
 }
