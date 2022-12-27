43f720f9d09a458c2d48ad492945001ca4931ccf
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-26 17:25:53 -1000

    updated design and added displaying random dog image using The Dog API

--- client/src/pages/LogInPage.jsx
+++ client/src/pages/LogInPage.jsx
@@ -1,6 +1,14 @@
 import axios from 'axios'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link, Navigate } from 'react-router-dom';
+import {
+    MDBBtn,
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBInput
+  }
+  from 'mdb-react-ui-kit';
 
 export default function LogInPage() {
     function getCookie(name) {
@@ -22,7 +30,17 @@ export default function LogInPage() {
     axios.defaults.headers.common['X-CSRFToken'] = csrftoken
     
     const [loggedIn, setLoggedIn] = useState(false)
+    const [dogPhoto, setDogPhoto] = useState("")
 
+    useEffect(()=>{
+        getLogin()
+    },[])
+
+    async function getLogin() {
+        let response = await axios.get("login")
+        setDogPhoto(response.data.dog_image)
+    }
+ 
     async function authenticateUser(event) {
         event.preventDefault()
         let email = document.getElementById('LogInEmail').value
@@ -43,14 +61,26 @@ export default function LogInPage() {
 
     return (
         <div className="LogInPage">
-        <h1>Welcome to Fetch!</h1><br/><br/>
-        <h5>Please log in</h5><br/>
-        <form onSubmit={authenticateUser}>
-            <label for="LogInEmail">email: </label><input id="LogInEmail" type="text" /><br/><br/>
-            <label for="LogInPassword">password: </label><input id="LogInPassword" type="password" /><br/><br/>
-            <input type="submit"></input>
-        </form><br/>
-        <p>Don't have an account?</p> <Link to="/signup">Sign Up!</Link>
+        <MDBContainer fluid>
+            <MDBRow>
+                <MDBCol sm='6'>
+                <div className='d-flex flex-row ps-5 pt-5'>
+                </div>
+                <div className='d-flex flex-column justify-content-center h-custom-2 w-75 pt-4'>
+                    <h1 className="fw-normal mb-3 ps-5 pb-3" style={{letterSpacing: '1px'}}>Welcome to Fetch!</h1>
+                    <MDBInput wrapperClass='mb-4 mx-5 w-100' label='Email address' id='LogInEmail' type='email' size="lg"/>
+                    <MDBInput wrapperClass='mb-4 mx-5 w-100' label='Password' id='LogInPassword' type='password' size="lg"/>
+                    <MDBBtn onClick={authenticateUser} className="mb-4 px-5 mx-5 w-100"  size='lg'>Login</MDBBtn>
+                    <p className='ms-5'>Don't have an account? <Link class="link-info" to="/signup">Sign Up!</Link></p>
+                </div>
+                </MDBCol>
+                <MDBCol sm='6' className='d-none d-sm-block px-0'>
+                <img src={dogPhoto}
+                // "https://mdbootstrap.com/img/Photos/Vertical/mountain2.webp"
+                    alt="Login image" className="w-100" style={{objectFit: 'cover', objectPosition: 'left'}} />
+                </MDBCol>
+            </MDBRow>
+        </MDBContainer>
         </div>
     )
 }
