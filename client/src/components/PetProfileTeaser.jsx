cfb65913465613d4cbd9523b8192352034bda8eb
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-26 17:18:07 -1000

    comment out profile image until it is working

--- client/src/components/PetProfileTeaser.jsx
+++ client/src/components/PetProfileTeaser.jsx
@@ -1,5 +1,13 @@
 import { useNavigate } from 'react-router-dom';
-import Button from 'react-bootstrap/Button'
+import {
+    MDBBtn,
+    MDBContainer,
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+  }
+  from 'mdb-react-ui-kit';
 import Card from 'react-bootstrap/Card'
 import ListGroup from 'react-bootstrap/ListGroup';
 
@@ -10,25 +18,29 @@ export default function PetProfileTeaser({pets}) {
     return (
     <div className="PetProfileTeaser">
     {pets.map((pet)=> (
-        <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={pet.profile_image} />
-        <Card.Body>
-            <Card.Title>{pet.name}</Card.Title>
-            <Card.Text>
-            DOB: {pet.birthdate}<br/>
-            {pet.gender}<br/>
+        <MDBContainer fluid className='d-flex align-items-center justify-content-center bg-image'>
+        <MDBCard className='m-5' style={{maxWidth: '600px'}}>
+        {/* <Card.Img variant="top" src={pet.profile_image} /> */}
+        <MDBCardBody className='px-5'>
+            <MDBCardTitle>{pet.name}</MDBCardTitle>
+            <MDBCardText>
+            {pet.gender} {pet.breed}<br/>
             spayed/neutered: {pet.spayed_neutered_status}<br/>
-            breed: {pet.breed}
-            </Card.Text>
-        </Card.Body>
+            birthdate: {pet.birthdate}<br/>
+            </MDBCardText>
+        </MDBCardBody>
         <ListGroup className="list-group-flush">
             <ListGroup.Item>personality: {pet.personality}</ListGroup.Item>
             <ListGroup.Item>{pet.name} likes {pet.likes}</ListGroup.Item>
         </ListGroup>
-        <Card.Body>
-            <Card.Link onClick={()=>navigate(`/editpetprofile${pet.id}`)}>edit profile</Card.Link>
-        </Card.Body>
-        </Card>
+        <MDBCardBody>
+            <MDBBtn onClick={()=>navigate(`/editpetprofile${pet.id}`)}>edit profile</MDBBtn><br/><br/>
+            <MDBBtn onClick={()=>navigate(`/viewmessages${pet.id}`)}>view messages</MDBBtn>
+        </MDBCardBody>
+        <div className='mask gradient-custom-3'></div>
+        </MDBCard>
+
+        </MDBContainer>
     ))} 
     </div>
     )
