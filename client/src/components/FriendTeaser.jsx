2367a73ab7cc6c9184b7b1bdcb6ba9bde00d0b9c
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-26 17:17:08 -1000

    comment out profile image until it is working

--- client/src/components/FriendTeaser.jsx
+++ client/src/components/FriendTeaser.jsx
@@ -1,6 +1,15 @@
 import { useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
+import {
+    MDBBtn,
+    MDBContainer,
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+  }
+  from 'mdb-react-ui-kit';
 
 export default function FriendTeaser({friends}) {
 
@@ -9,17 +18,22 @@ export default function FriendTeaser({friends}) {
     return (
     <div className="FriendTeaser">
     {friends.map((friend)=> (
-     <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={friend.profile_image} />
-        <Card.Body>
-            <Card.Title>{friend.name}</Card.Title>
-            <Card.Text>
-            {friend.gender} / {friend.breed}<br/>
-            spayed/neutered: {friend.spayed_neutered_status}<br/>
-            </Card.Text>
-            <Button variant="primary" onClick={()=>navigate(`/viewfriend${friend.id}`)}>view profile</Button>
-        </Card.Body>
-        </Card>
+        <MDBContainer fluid className='d-flex align-items-center justify-content-center bg-image'>
+            <MDBCard className='m-5' style={{maxWidth: '600px'}}>
+            {/* <Card.Img variant="top" src={friend.profile_image} /> */}
+            <MDBCardBody className='px-5'>
+                <MDBCardTitle>{friend.name}</MDBCardTitle>
+                <MDBCardText>
+                {friend.gender} {friend.breed}<br/>
+                spayed/neutered: {friend.spayed_neutered_status}<br/>
+                </MDBCardText>
+            </MDBCardBody>
+            <MDBCardBody>
+                <MDBBtn onClick={()=>navigate(`/viewfriend${friend.id}`)}>view profile</MDBBtn><br/><br/>
+            </MDBCardBody>
+            <div className='mask gradient-custom-3'></div>
+            </MDBCard>
+        </MDBContainer>
     ))} 
     </div>
     )
