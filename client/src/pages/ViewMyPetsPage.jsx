3cdbfc3a1e758ffc8404926b15f579b93f174d75
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-12-26 17:27:28 -1000

    removed useEffect for myPets state, did not need

--- client/src/pages/ViewMyPetsPage.jsx
+++ client/src/pages/ViewMyPetsPage.jsx
@@ -7,7 +7,7 @@ export default function ViewMyPetsPage(){
 
     const [responseData, setResponseData] = useState([])
     const [myPets, setMyPets] = useState([])
-    const [availablePets, setAvailablePets] = useState(true)
+    const [availablePets, setAvailablePets] = useState(false)
 
     useEffect(()=>{
         getMyPets()
@@ -19,34 +19,30 @@ export default function ViewMyPetsPage(){
     }
 
     useEffect(()=>{
-        console.log(responseData)
-        setMyPets(responseData.map((pet)=>{
-            return {
-                id: pet.pk,
-                owner: pet.fields.user_pet,
-                name: pet.fields.name,
-                birthdate: pet.fields.birthdate,
-                breed: pet.fields.breed,
-                gender: pet.fields.gender,
-                spayed_neutered_status: pet.fields.spayed_neutered,
-                profile_image: pet.fields.profile_image,
-                city: pet.fields.city,
-                personality: pet.fields.personality,
-                likes: pet.fields.likes
-            }
-        }))
+        if (responseData.length > 0) {
+            setAvailablePets(true)
+            setMyPets(responseData.map((pet)=>{
+                return {
+                    id: pet.pk,
+                    owner: pet.fields.user_pet,
+                    name: pet.fields.name,
+                    birthdate: pet.fields.birthdate,
+                    breed: pet.fields.breed,
+                    gender: pet.fields.gender,
+                    spayed_neutered_status: pet.fields.spayed_neutered,
+                    profile_image: pet.fields.profile_image,
+                    city: pet.fields.city,
+                    personality: pet.fields.personality,
+                    likes: pet.fields.likes
+                }
+            }))
+        } 
     },[responseData])
-
-    useEffect(()=>{
-        if (!myPets) {
-            setAvailablePets(false)
-        }
-    },[myPets])
 
     return (
         <div className="ViewMyPetPage">
             <NavBar /><br/>
-            {availablePets ? <PetProfileTeaser pets={myPets}/> : <p>you haven't added any pets.</p>}
+            {availablePets ? <PetProfileTeaser pets={myPets}/> : <h3>you haven't added any pets yet!</h3>}
         </div>
     )
 }
